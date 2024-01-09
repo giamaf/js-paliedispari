4a8de0bc206ff2f9885aeba2615f14256a382db4
giamaf <gianluca.maffucci@hotmail.com>
2024-01-09 16:11:42 +0100

    js_get DOM's elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 console.log('JS OK');
 
+//todo ------------------------------------------ \\
+
+//! Palindroma
+//* Chiedere all’utente di inserire una parola (con un prompt)
+//* Creare una funzione per capire se la parola inserita è palindroma
+//* Stampiamo il risultato in console
+
+//! Bonus
+//* Raccogliere le informazioni dall'utente usando form, input, button in pagina invece che coi prompt.
+//* Stampare i risultati in pagina, invece che in console
+
+// Recupero gli elementi dal DOM
+const formBox = document.getElementById('form-box');
+const textInput = document.getElementById('text-input');
+const verifyButton = document.getElementById('verify-button');
+const result = document.getElementById('result');
 
 
